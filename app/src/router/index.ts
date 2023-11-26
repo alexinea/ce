ee0e30bac79dc8bed25b1f1898d7f97985f72031
AlexLEWIS <forerunner@outlook.com>
2023-11-26 21:13:45 +0800

    Add hooks and utils.

--- app/src/router/index.ts
+++ app/src/router/index.ts
@@ -1,14 +1,23 @@
-import { createRouter, createWebHistory } from "vue-router";
+import {createRouter, createWebHistory, type RouteRecordRaw} from "vue-router";
+
+const baseRoutes: RouteRecordRaw[] = [
+    {
+        path: "/",
+        name: "home",
+        component: () => import("../views/HomeView.vue")
+    },
+    {
+        path: "/:pathMatch(.*)*",
+        name: "all",
+        component: () => import("../views/Page404.vue")
+    }
+];
+
+//const routes = baseRoutes;//.concat(peopleRoutes, projectRoutes, mediaRoutes, ruleRoutes, aboutRoutes);
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/:pathMatch(.*)*",
-      name: "all",
-      component: () => import("../views/Home.vue"),
-    }
-  ]
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: baseRoutes
 });
 
 export default router;
