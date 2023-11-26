8ab588e22a5d292b82c4473a5f24cab9fbbbd7e2
AlexLEWIS <forerunner@outlook.com>
2023-11-26 21:20:36 +0800

    Update views

---
+++ app/src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import {setTitle} from "@/hooks/usePageToolkits";
+
+setTitle("Home");
+
+</script>
+
+<template>
+
+  <div class="responsive-width text-2xl text-black dark:text-white h-40">Event Home</div>
+
+</template>
+
+<style scoped lang="css">
+
+</style>
