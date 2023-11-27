779fa5eeb9a8c7a7c34cc2fd8ff565dc26eb1cc1
AlexLEWIS <forerunner@outlook.com>
2023-11-27 15:25:43 +0800

    Add all contribute pages.

--- app/src/router/index.ts
+++ app/src/router/index.ts
@@ -7,6 +7,26 @@ const baseRoutes: RouteRecordRaw[] = [
         component: () => import("../views/HomeView.vue")
     },
     {
+        path: "/project-join-ncc",
+        name: "project-join-ncc",
+        component: () => import("../views/ProjectJoinsNCC.vue")
+    },
+    {
+        path: "/contribute-to-project",
+        name: "contribute-to-project",
+        component: () => import("../views/Contribute2Project.vue")
+    },
+    {
+        path: "/attend-ncc",
+        name: "attend-ncc",
+        component: () => import("../views/AttendTheNCC.vue")
+    },
+    {
+        "path": "/make-ncc-better",
+        "name": "make-ncc-better",
+        component: () => import("../views/HelpTheNCCBetter.vue")
+    },
+    {
         path: "/:pathMatch(.*)*",
         name: "all",
         component: () => import("../views/Page404.vue")
